ff84ec21eceda93786ada084c57d93fcce2062f6
shikhar <shikhar.sharma@stonybrook.edu>
2017-04-25 18:49:40 -0400

    filenames functions added

---
+++ src/Filenames.py
@@ -0,0 +1,57 @@
+from config import *
+import os
+
+def getBaseExperimentName(config):
+    cachingStr = "cache-on" if config['caching'] else "cache-off"
+    return cachingStr + "_nReq-" + str(config['nReq']) + "_subset-" + str(config['subset']) + "_levels-" + str(config['levels'])
+
+def createAllFolders(config):
+    createFolderIfNotExists(getBaseStatsFolderName(config))
+    createFolderIfNotExists(getBaseStatsFolderName(config) + chordFolderName + "/")
+    createFolderIfNotExists(getBaseStatsFolderName(config) + hierarchicalFolderName + "/")
+
+    createFolderIfNotExists(getBaseLogsFolderName(config))
+    createFolderIfNotExists(getBaseLogsFolderName(config) + chordFolderName + "/")
+    createFolderIfNotExists(getBaseLogsFolderName(config) + hierarchicalFolderName + "/")
+    
+    createFolderIfNotExists(getBaseGraphsFolderName(config))
+
+# ------------  stats ------------------
+def getBaseStatsFolderName(config):
+    return statsFolder + "/" + config['name'] + "/"
+def getStatsFolderName(config):
+    return getBaseStatsFolderName(config) + config['type'] + "/"
+def getStatsFilename(config):
+    return getStatsFolderName(config) + getBaseExperimentName(config) + ".txt"
+def getStatsPickleFilename(config):
+    return getStatsFolderName(config) + getBaseExperimentName(config) + ".pickle"
+
+
+# ------------  logs ------------------
+def getBaseLogsFolderName(config):
+    return logsFolder + "/" + config['name'] + "/"
+def getLogsFolderName(config):
+    return getBaseLogsFolderName(config) + config['type'] + "/"
+def getClientLogFilename(config):
+    return getLogsFolderName(config) + getBaseExperimentName(config) + ".client.txt"
+def getMasterLogFilename(config):
+    return getLogsFolderName(config) + getBaseExperimentName(config) + ".master.txt"
+
+
+# ------------  graphs ------------------
+def getBaseGraphsFolderName(config):
+    return graphsFolder + "/" + config['name'] + "/"
+def getGraphsFolderName(config):
+    return getBaseGraphsFolderName(config)
+def getThroughputGraphFilename(config):
+    return getGraphsFolderName(config) + "throughput.png"
+def getLatencyGraphFilename(config):
+    return getGraphsFolderName(config) + "latency.png"
+def getMessagePerRequestGraphFilename(config):
+    return getGraphsFolderName(config) + "messagesPerRequest.png"
+
+
+def createFolderIfNotExists(folderName):
+    directory = os.path.dirname(folderName)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
