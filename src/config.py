5d5a4ed330d038a9f10afa4d92c5b747a42892e5
shikhar <shikhar.sharma@stonybrook.edu>
2017-04-26 00:21:23 -0400

    removed domainCardinalities from configs.json; moved to config.py as a permanent config

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
 statsFolder = "stats"
 graphsFolder = "graphs"
 logsFolder = "logs"
+domainNamesFolder = "domainNames"
 configsFolder = "configs"
 chordFolderName = "chord"
 hierarchicalFolderName = "hierarchical"
+nTLD = 10
 
 chord = "chord"
 hierarchical = "hierarchical"
