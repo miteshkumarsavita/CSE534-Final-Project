1bfd646fb7519133ab0f2608fef27e6ba94a67fe
shikhar <shikhar.sharma@stonybrook.edu>
2017-04-26 00:27:01 -0400

    moved nTLD to config#.json

--- src/config.py
+++ src/config.py
@@ -5,7 +5,6 @@ domainNamesFolder = "domainNames"
 configsFolder = "configs"
 chordFolderName = "chord"
 hierarchicalFolderName = "hierarchical"
-nTLD = 10
 
 chord = "chord"
 hierarchical = "hierarchical"
