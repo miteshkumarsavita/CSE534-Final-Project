fb18803b272c20837ef42f6837a1cd595f0cca52
Mitesh Kumar Savita <miteshkumarsavita@gmail.com>
2017-05-08 16:19:12 -0400

    fix subset experiment bug

--- src/RequestGenerator.py
+++ src/RequestGenerator.py
@@ -11,7 +11,7 @@ class RequestGenerator:
         self.randomizer.seed(seed)
 
         # only take the first {subset} fraction of requests
-        self.domainNames = domainNames['domainNames'][:len(domainNames['domainNames'])*subset]
+        self.domainNames = domainNames['domainNames'][:int(len(domainNames['domainNames'])*subset)]
     
     def getNext(self):
         query = {}
